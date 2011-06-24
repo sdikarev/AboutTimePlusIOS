32bf237a2f19bd4460428f6a23c925fa72bdfdad
Sergey Dikarev <sdikarev@gmail.com>
2011-06-24 10:10:19 +0400

    First changes

---
+++ MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  AboutTimePlus
+//
+//  Created by Sergey Dikarev on 24.06.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MainViewController : UIViewController {
+    
+}
+
+@end
